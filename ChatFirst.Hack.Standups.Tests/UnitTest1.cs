0f246f1db7ba4280fcb78befb5ac1fc56f482863
Pavel Osadchuk <xakz.pc@gmail.com>
2016-10-16 02:17:13 +0300

    Add UnitTests Project

---
+++ ChatFirst.Hack.Standups.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChatFirst.Hack.Standups.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
